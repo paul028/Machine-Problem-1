e1c4b4ab308bda216bbd4e03a12dcbb25794749a
paul028 <paulvincentnonat@gmail.com>
2019-11-13 10:00:30 +0800

    Fix Runtime

--- prob3.py
+++ prob3.py
@@ -6,6 +6,7 @@ Created on Tue Nov 12 23:35:00 2019
 """
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 start_time = time.time()
 n=60000
 k=12489
@@ -14,26 +15,28 @@ p=0.5
 
 def ln_factorial(n):
     ln_fac=0
+    print("Calculating  ln("+str(n)+"!) from the sum of ln(x) from 1 to"+str(n))
     for x in range(n):
-        print(x+1)
-        ln_fac= ln_fac + np.log(x+1)
+        log_x=np.log(x+1)
+        print("ln("+str(x+1)+") ="+str(log_x))
+        ln_fac= ln_fac + log_x
     
+    print("ln("+str(n)+"!) ="+str(ln_fac))
     return ln_fac;
 
-def binpmf(n,p,k)
+def binpmf(n,p,k):
     p_X_k=np.zeros(n-k)
+    print("Calculating ln("+str(n)+")")
     ln_n_fac=ln_factorial(n)
     z=0
     for k in range(k,n+1,1):
         
-        print("Calculating natural logarithm of "+str(k)+"!")
         ln_k_fac=ln_factorial(k)
-        print("Calculating natural logarithm of "+str(n-k)+"!")
-        ln_n-k_fac=ln_factorial(n-k)
-        ln_X_p_k= ln_n_fac - ln_k_fac - ln_n-k_fac + (k*np.log(p)) + ((n-k)*np.log(1-p))
+        ln_nk_fac=ln_factorial(n-k)
+        ln_X_p_k= ln_n_fac - ln_k_fac - ln_nk_fac + (k*np.log(p)) + ((n-k)*np.log(1-p))
         
         print("Calculating PMF at k="+str(k))
-        p_x_k[z]= np.exp(ln_X_p_k)
+        p_X_k[z]= np.exp(ln_X_p_k)
         z=z+1
         
     return p_X_k;
@@ -41,7 +44,7 @@ def binpmf(n,p,k)
 prob_X_K=binpmf(n,p,k)
 print("Calculation Time: %s seconds "(time.time() - start_time))
 
-plt.plot(prob_n)
+plt.plot(prob_X_K)
 plt.ylabel(r'$\P_k(K)$')
 plt.xlabel('k')
 plt.title('Probability Curve')
