4a762d9804cccaaaf35cb6444e5cd921469799b3
paul028 <paulvincentnonat@gmail.com>
2019-11-17 23:32:16 +0800

    define constant_set function

--- prob4.py
+++ prob4.py
@@ -7,3 +7,12 @@ Created on Sun Nov 17 13:16:14 2019
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+def constant_set(n):
+    reciprocal_cn=0 #reciprocal of cn
+    for i in range(n):
+        reciprocal_cn= reciprocal_cn+ (1/(i+1))
+    
+    cn = 1/reciprocal_cn
+    return cn;
+
